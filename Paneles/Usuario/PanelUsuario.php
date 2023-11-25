cc044871f5d7ce5d3c33a334c9f49c0eb12a03dc
kdgarzon <karendanielagarzon11@gmail.com>
2023-11-24 23:13:41 -0500

    Se añade barra de navegación vertical

--- Paneles/Usuario/PanelUsuario.php
+++ Paneles/Usuario/PanelUsuario.php
@@ -23,19 +23,49 @@
     <title>Inicio</title>
 </head>
 <body class="body_Usuario">
+    
     <div class="panel_usuario">
         <img src="../../Imagenes/Panel principal usuario/pavo real.jpg" alt="" class="fondo_paneles">
-        <img src="../../Imagenes/Iconos/TresLineasBlancas.png" alt="" class="tresLineas">
+        <div id="NavegacionContainer" onclick="toggleNavbar()">
+            <img src="../../Imagenes/Iconos/TresLineasBlancas.png" alt="" class="tresLineas">
+        </div>
         <div class="bienvenida">
             <h1 class="usu">BIENVENIDO A ZOOLOGICS</h1>
             <hr>
         </div>
     </div>
+    <?php include('../../config/headerUsuario.php'); ?>
     <footer>
         <p>&copy;2023. Todos los derechos reservados. Elaborado por Karen Garzon :)</p>
     </footer>
     <!--<a href="../../config/CerrarSesion.php">Cerrar sesión</a>-->
 
+    <script>
+        var navbar = document.getElementById("navbarVertical");
+
+        function toggleNavbar() {
+            if (navbar.style.display === "none" || navbar.style.display === "") {
+                mostrarNavbar();
+            } else {
+                ocultarNavbar();
+            }
+        }
+
+        function mostrarNavbar() {
+            navbar.style.display = "block";
+            setTimeout(function () {
+                navbar.classList.remove("navbarHidden");
+            }, 0); // Espera un ciclo de eventos antes de agregar la clase para activar la transición
+        }
+
+        function ocultarNavbar() {
+            navbar.classList.add("navbarHidden");
+            setTimeout(function () {
+                navbar.style.display = "none";
+            }, 500); // Espera 0.5 segundos (duración de la transición) antes de ocultar definitivamente
+        }
+        
+    </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" 
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" 
         crossorigin="anonymous"></script>
---
+++ config/headerUsuario.php
@@ -0,0 +1,35 @@
+<head>
+    <link rel="stylesheet" href="../../Estilos/Paneles.css">
+</head>
+<div id="navbarVertical">
+    <div class="logo">
+        <img src="../../Imagenes/Iconos/tucan.png" alt="Logo" width="70px" height="70px" class="d-inline-block align-text-top">
+        <h1 class="titulo_logotipo">Zoologics</h1>
+    </div>
+    <div class="regresar" onclick="ocultarNavbar()">
+        <img src="../../Imagenes/Iconos/atras.png" alt="" width="58px" height="52px">
+    </div>
+    <h3>INICIO</h3>
+    <hr>
+    <a href="PanelUsuario.php">Página principal</a>
+    <h3>ZOOLOGICOS</h3>
+    <hr>
+    <a href="../Zoologicos/PanelSanDiego.php">Zoológico de San Diego</a>
+    <a href="../Zoologicos/PanelSingapur.php">Zoológico de Singapur</a>
+    <a href="../Zoologicos/PanelToronto.php">Zoológico de Toronto</a>
+    <a href="../Zoologicos/PanelLoroParque.php">Zoológico Loro Parque</a>
+    <a href="../Zoologicos/PanelSchonbrunn.php">Zoológico de Schonbrunn</a>
+    <a href="../Zoologicos/PanelLondres.php">Zoológico de Londres</a>
+    <a href="../Zoologicos/PanelBerlin.php">Zoológico de Berlín</a>
+    <a href="../Zoologicos/PanelBronx.php">Zoológico del Bronx</a>
+    <a href="../Zoologicos/PanelChapultepec.php">Zoológico de Chapultepec</a>
+    <h3>ACUARIOS</h3>
+    <hr>
+    <a href="../Zoologicos/PanelTwoOceans.php">Acuario Two Oceans</a>
+    <div class="btn_cerrar">
+        <a href="../../Index/PaginaPrincipal.php">
+            <img src="../../Imagenes/Iconos/cerrar-sesion.png" alt="" width="32px" height="32px">
+            Cerrar Sesion
+        </a>
+    </div>
+</div>
