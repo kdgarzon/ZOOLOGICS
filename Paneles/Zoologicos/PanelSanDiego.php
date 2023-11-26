202a8ed5348cb6df507dbd609eea78ab1af11668
kdgarzon <karendanielagarzon11@gmail.com>
2023-11-25 19:17:11 -0500

    Se genera FrontEnd zoo de San Diego

--- Paneles/Zoologicos/PanelSanDiego.php
+++ Paneles/Zoologicos/PanelSanDiego.php
@@ -20,24 +20,23 @@
         rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" 
         crossorigin="anonymous">
     <link rel="stylesheet" href="../../Estilos/Paneles.css">
+    <link rel="stylesheet" href="../../Estilos/PestañasZoo.css">
     <title>Inicio</title>
 </head>
 <body>
     <div class="barra_superior">
         <img src="../../Imagenes/San Diego/Imagenes zoo/1.jpg" alt="San Diego">
-        <div class="cont">
-            <div class="logo">
-                <img src="../../Imagenes/Iconos/tucan.png" alt="Logo" width="40px" height="40px" class="d-inline-block align-text-top">
-                <h1 class="titulo_logotipo">Zoologics</h1>
-            </div>
-            <img src="../../Imagenes/Iconos/TresLineasBlancas.png" alt="Barra desplegable">
-        </div>
+        <?php include('../../config/headerAuxiliar.php'); ?>
         <div class="cont_dos">
-            <h1 class="titulo_imagen">Zoológico de San Diego</h1>
-            <h3 class="apartado">
-                Situado en San Diego, California, fue creado en 1915 y cuenta con unos 4000 ejemplares 
-                de más de 800 especies distintas.
-            </h3>
+            <div class="uno">
+                <h1 class="titulo_imagen">ZOOLOGICO DE SAN DIEGO</h1>
+            </div>
+            <div class="dos">
+                <h3 class="apartado">
+                    Situado en San Diego, California, fue creado en 1915 y cuenta con unos 4000 ejemplares 
+                    de más de 800 especies distintas.
+                </h3>
+            </div>
         </div>
     </div>
     <div class="texto_cuerpo">
@@ -90,160 +89,164 @@
     </div>
     <div class = "conservacion">
         <h2 class="titulos_contenido_dos">CONSERVACION DE ESPECIES</h2>
-        <div class = "circulo_tarjeta">
-            <div class="cirImg">
-                <img src="../../Imagenes/San Diego/islas del pacifico/islas del pacifico.jpg" alt="Islas del pacifico" height="216px" width="216px">
+        <div class="tarjetas">
+            <div class = "circulo_tarjeta">
+                <div class="cirImg">
+                    <img src="../../Imagenes/San Diego/islas del pacifico/islas del pacifico.jpg" alt="Islas del pacifico" height="216px" width="216px">
+                </div>
+                <div class="cirText">
+                    <h4 class="cirTitulo">Islas del pacífico</h4>
+                    <p class="tecirculo">
+                        Programa de Conservación de Aves en Peligro de Hawaii, que opera los Centros de 
+                        Conservación de Aves de Keauhou y Maui.
+                    </p>
+                </div>
             </div>
-            <div class="cirText">
-                <h4 class="cirTitulo">Islas del pacífico</h4>
-                <p class="tecirculo">
-                    Programa de Conservación de Aves en Peligro de Hawaii, que opera los Centros de 
-                    Conservación de Aves de Keauhou y Maui.
-                </p>
+            <div class = "circulo_tarjeta">
+                <div class="cirImg">
+                    <img src="../../Imagenes/San Diego/islas del pacifico/sur oeste.jpg" alt="Sur oeste" height="216px" width="216px">
+                </div>
+                <div class="cirText">
+                    <h4 class="cirTitulo">Sur Oeste</h4>
+                    <p class="tecirculo">
+                        Gestionamos una reserva de biodiversidad de 900 acres adyacente al San Diego Zoo 
+                        Safari Park, que sustenta una diversidad de matorrales costeros y vida silvestre de 
+                        chaparral.
+                    </p>
+                </div>
             </div>
-        </div>
-        <div class = "circulo_tarjeta">
-            <div class="cirImg">
-                <img src="../../Imagenes/San Diego/islas del pacifico/sur oeste.jpg" alt="Sur oeste" height="216px" width="216px">
+            <div class = "circulo_tarjeta">
+                <div class="cirImg">
+                    <img src="../../Imagenes/San Diego/islas del pacifico/amazonia.jpg" alt="Amazonia" height="216px" width="216px">
+                </div>
+                <div class="cirText">
+                    <h4 class="cirTitulo">Amazonía</h4>
+                    <p class="tecirculo">
+                        Colaboramos con expertos en conservación en la Amazonía peruana para que la vida 
+                        silvestre pueda seguir prosperando.
+                    </p>
+                </div>
             </div>
-            <div class="cirText">
-                <h4 class="cirTitulo">Sur Oeste</h4>
-                <p class="tecirculo">
-                    Gestionamos una reserva de biodiversidad de 900 acres adyacente al San Diego Zoo 
-                    Safari Park, que sustenta una diversidad de matorrales costeros y vida silvestre de 
-                    chaparral.
-                </p>
+            <div class = "circulo_tarjeta">
+                <div class="cirImg">
+                    <img src="../../Imagenes/San Diego/islas del pacifico/bosque africano.jpg" alt="bosque africano" height="216px" width="216px">
+                </div>
+                <div class="cirText">
+                    <h4 class="cirTitulo">Bosque Africano</h4>
+                    <p class="tecirculo">
+                        Colaboramos con comunidades locales para conservar la biodiversidad en el bosque de 
+                        Ebo de Camerún, uno de los últimos bosques intactos que quedan en toda África Central.
+                    </p>
+                </div>
             </div>
-        </div>
-        <div class = "circulo_tarjeta">
-            <div class="cirImg">
-                <img src="../../Imagenes/San Diego/islas del pacifico/amazonia.jpg" alt="Amazonia" height="216px" width="216px">
+            <div class = "circulo_tarjeta">
+                <div class="cirImg">
+                    <img src="../../Imagenes/San Diego/islas del pacifico/sabana.jpg" alt="sabana" height="216px" width="216px">
+                </div>
+                <div class="cirText">
+                    <h4 class="cirTitulo">Sabana</h4>
+                    <p class="tecirculo">
+                        En la sabana africana del norte de Kenia, colaboramos con innovadores en conservación 
+                        para proteger la vida silvestre, incluidos los elefantes y rinocerontes africanos.
+                    </p>
+                </div>
             </div>
-            <div class="cirText">
-                <h4 class="cirTitulo">Amazonía</h4>
-                <p class="tecirculo">
-                    Colaboramos con expertos en conservación en la Amazonía peruana para que la vida 
-                    silvestre pueda seguir prosperando.
-                </p>
+            <div class = "circulo_tarjeta">
+                <div class="cirImg">
+                    <img src="../../Imagenes/San Diego/islas del pacifico/oceanos.jpg" alt="oceanos" height="216px" width="216px">
+                </div>
+                <div class="cirText">
+                    <h4 class="cirTitulo">Oceanos</h4>
+                    <p class="tecirculo">
+                        Colaboramos con una variedad de innovadores para salvaguardar dos de los depredadores 
+                        más emblemáticos del océano.
+                    </p>
+                </div>
             </div>
-        </div>
-        <div class = "circulo_tarjeta">
-            <div class="cirImg">
-                <img src="../../Imagenes/San Diego/islas del pacifico/bosque africano.jpg" alt="bosque africano" height="216px" width="216px">
+            <div class = "circulo_tarjeta">
+                <div class="cirImg">
+                    <img src="../../Imagenes/San Diego/islas del pacifico/selva asiatica.jpg" alt="selva asiatica" height="216px" width="216px">
+                </div>
+                <div class="cirText">
+                    <h4 class="cirTitulo">Selva Asiática</h4>
+                    <p class="tecirculo">
+                        Colaboramos con científicos locales para conservar la vida silvestre única de la región 
+                        y apoyamos los esfuerzos para abogar por el aceite de palma sostenible.
+                    </p>
+                </div>
             </div>
-            <div class="cirText">
-                <h4 class="cirTitulo">Bosque Africano</h4>
-                <p class="tecirculo">
-                    Colaboramos con comunidades locales para conservar la biodiversidad en el bosque de 
-                    Ebo de Camerún, uno de los últimos bosques intactos que quedan en toda África Central.
-                </p>
-            </div>
-        </div>
-        <div class = "circulo_tarjeta">
-            <div class="cirImg">
-                <img src="../../Imagenes/San Diego/islas del pacifico/sabana.jpg" alt="sabana" height="216px" width="216px">
-            </div>
-            <div class="cirText">
-                <h4 class="cirTitulo">Sabana</h4>
-                <p class="tecirculo">
-                    En la sabana africana del norte de Kenia, colaboramos con innovadores en conservación 
-                    para proteger la vida silvestre, incluidos los elefantes y rinocerontes africanos.
-                </p>
-            </div>
-        </div>
-        <div class = "circulo_tarjeta">
-            <div class="cirImg">
-                <img src="../../Imagenes/San Diego/islas del pacifico/oceanos.jpg" alt="oceanos" height="216px" width="216px">
-            </div>
-            <div class="cirText">
-                <h4 class="cirTitulo">Oceanos</h4>
-                <p class="tecirculo">
-                    Colaboramos con una variedad de innovadores para salvaguardar dos de los depredadores 
-                    más emblemáticos del océano.
-                </p>
-            </div>
-        </div>
-        <div class = "circulo_tarjeta">
-            <div class="cirImg">
-                <img src="../../Imagenes/San Diego/islas del pacifico/selva asiatica.jpg" alt="selva asiatica" height="216px" width="216px">
-            </div>
-            <div class="cirText">
-                <h4 class="cirTitulo">Selva Asiática</h4>
-                <p class="tecirculo">
-                    Colaboramos con científicos locales para conservar la vida silvestre única de la región 
-                    y apoyamos los esfuerzos para abogar por el aceite de palma sostenible.
-                </p>
-            </div>
-        </div>
-        <div class = "circulo_tarjeta">
-            <div class="cirImg">
-                <img src="../../Imagenes/San Diego/islas del pacifico/bosque australiano.jpg" alt="bosque australiano" height="216px" width="216px">
-            </div>
-            <div class="cirText">
-                <h4 class="cirTitulo">Bosque Australiano</h4>
-                <p class="tecirculo">
-                    Trabajamos estrechamente con conservacionistas en Australia porque reconocemos la 
-                    importancia de preservar la historia natural y cultural representada en esta región.
-                </p>
+            <div class = "circulo_tarjeta">
+                <div class="cirImg">
+                    <img src="../../Imagenes/San Diego/islas del pacifico/bosque australiano.jpg" alt="bosque australiano" height="216px" width="216px">
+                </div>
+                <div class="cirText">
+                    <h4 class="cirTitulo">Bosque Australiano</h4>
+                    <p class="tecirculo">
+                        Trabajamos estrechamente con conservacionistas en Australia porque reconocemos la 
+                        importancia de preservar la historia natural y cultural representada en esta región.
+                    </p>
+                </div>
             </div>
         </div>
     </div>
     <div class="galeria_animales">
         <h2 class="titulos_contenido_tres">ANIMALES MAS DESTACADOS</h2>
-        <div class="foto">
-            <img src="../../Imagenes/Animales de San Diego/Gorila.jpg" alt="Gorila" width="290px" height="290px">
-            <a href="https://sandiegozoowildlifealliance.org/species/gorilla">Gorila</a>
-        </div>
-        <div class="foto">
-            <img src="../../Imagenes/Animales de San Diego/Jaguar.jpg" alt="Jaguar" width="290px" height="290px">
-            <a href="https://sandiegozoowildlifealliance.org/species/jaguar">Jaguar</a>
-        </div>
-        <div class="foto">
-            <img src="../../Imagenes/Animales de San Diego/Tigre.jpg" alt="Tigre" width="290px" height="290px">
-            <a href="https://sandiegozoowildlifealliance.org/species/tiger">Tigre</a>
-        </div>
-        <div class="foto">
-            <img src="../../Imagenes/Animales de San Diego/ornitorrinco.png" alt="Ornitorrinco" width="290px" height="290px">
-            <a href="https://sandiegozoowildlifealliance.org/species/platypus">Ornitorrinco</a>
-        </div>
-        <div class="foto">
-            <img src="../../Imagenes/Animales de San Diego/Koala.jpg" alt="Koala" width="290px" height="290px">
-            <a href="https://sandiegozoowildlifealliance.org/species/koala">Koala</a>
-        </div>
-        <div class="foto">
-            <img src="../../Imagenes/Animales de San Diego/oso polar.jpg" alt="Oso polar" width="290px" height="290px">
-            <a href="https://sandiegozoowildlifealliance.org/species/polar-bear">Oso polar</a>
-        </div>
-        <div class="foto">
-            <img src="../../Imagenes/Animales de San Diego/pinguino.jpg" alt="Pinguino" width="290px" height="290px">
-            <a href="https://sandiegozoowildlifealliance.org/species/penguin">Pinguino</a>
-        </div>
-        <div class="foto">
-            <img src="../../Imagenes/Animales de San Diego/alala.jpg" alt="Alalá" width="290px" height="290px">
-            <a href="https://sandiegozoowildlifealliance.org/species/alala">Alalá</a>
-        </div>
-        <div class="foto">
-            <img src="../../Imagenes/Animales de San Diego/elefante.jpg" alt="Elefante" width="290px" height="290px">
-            <a href="https://sandiegozoowildlifealliance.org/species/elephant">Elefante</a>
-        </div>
-        <div class="foto">
-            <img src="../../Imagenes/Animales de San Diego/rinoceronte.jpg" alt="Rinoceronte" width="290px" height="290px">
-            <a href="https://sandiegozoowildlifealliance.org/species/rhino">Rinoceronte</a>
-        </div>
-        <div class="foto">
-            <img src="../../Imagenes/Animales de San Diego/tortuga del desierto.jpg" alt="Tortuga" width="290px" height="290px">
-            <a href="https://sandiegozoowildlifealliance.org/species/desert-tortoise">Tortuga</a>
-        </div>
-        <div class="foto">
-            <img src="../../Imagenes/Animales de San Diego/búho.jpg" alt="Búho Llanero" width="290px" height="290px">
-            <a href="https://sandiegozoowildlifealliance.org/species/burrowing-owl">Búho Llanero</a>
+        <div class="fotos">
+            <div class="foto">
+                <img src="../../Imagenes/Animales de San Diego/Gorila.jpg" alt="Gorila" width="290px" height="290px">
+                <a href="https://sandiegozoowildlifealliance.org/species/gorilla">Gorila</a>
+            </div>
+            <div class="foto">
+                <img src="../../Imagenes/Animales de San Diego/Jaguar.jpg" alt="Jaguar" width="290px" height="290px">
+                <a href="https://sandiegozoowildlifealliance.org/species/jaguar">Jaguar</a>
+            </div>
+            <div class="foto">
+                <img src="../../Imagenes/Animales de San Diego/Tigre.jpg" alt="Tigre" width="290px" height="290px">
+                <a href="https://sandiegozoowildlifealliance.org/species/tiger">Tigre</a>
+            </div>
+            <div class="foto">
+                <img src="../../Imagenes/Animales de San Diego/ornitorrinco.png" alt="Ornitorrinco" width="290px" height="290px">
+                <a href="https://sandiegozoowildlifealliance.org/species/platypus">Ornitorrinco</a>
+            </div>
+            <div class="foto">
+                <img src="../../Imagenes/Animales de San Diego/Koala.jpg" alt="Koala" width="290px" height="290px">
+                <a href="https://sandiegozoowildlifealliance.org/species/koala">Koala</a>
+            </div>
+            <div class="foto">
+                <img src="../../Imagenes/Animales de San Diego/oso polar.jpg" alt="Oso polar" width="290px" height="290px">
+                <a href="https://sandiegozoowildlifealliance.org/species/polar-bear">Oso polar</a>
+            </div>
+            <div class="foto">
+                <img src="../../Imagenes/Animales de San Diego/pinguino.jpg" alt="Pinguino" width="290px" height="290px">
+                <a href="https://sandiegozoowildlifealliance.org/species/penguin">Pinguino</a>
+            </div>
+            <div class="foto">
+                <img src="../../Imagenes/Animales de San Diego/alala.jpg" alt="Alalá" width="290px" height="290px">
+                <a href="https://sandiegozoowildlifealliance.org/species/alala">Alalá</a>
+            </div>
+            <div class="foto">
+                <img src="../../Imagenes/Animales de San Diego/elefante.jpg" alt="Elefante" width="290px" height="290px">
+                <a href="https://sandiegozoowildlifealliance.org/species/elephant">Elefante</a>
+            </div>
+            <div class="foto">
+                <img src="../../Imagenes/Animales de San Diego/rinoceronte.jpg" alt="Rinoceronte" width="290px" height="290px">
+                <a href="https://sandiegozoowildlifealliance.org/species/rhino">Rinoceronte</a>
+            </div>
+            <div class="foto">
+                <img src="../../Imagenes/Animales de San Diego/tortuga del desierto.jpg" alt="Tortuga" width="290px" height="290px">
+                <a href="https://sandiegozoowildlifealliance.org/species/desert-tortoise">Tortuga</a>
+            </div>
+            <div class="foto">
+                <img src="../../Imagenes/Animales de San Diego/búho.jpg" alt="Búho Llanero" width="290px" height="290px">
+                <a href="https://sandiegozoowildlifealliance.org/species/burrowing-owl" id="buho">Búho Llanero</a>
+            </div>
         </div>
     </div>
     <!--<a href="../../config/CerrarSesion.php">Cerrar sesión</a>-->
 
-    <footer>
+    <div class="footer">
         <p>&copy;2023. Todos los derechos reservados. Elaborado por Karen Garzon :)</p>
-    </footer>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" 
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" 
         crossorigin="anonymous"></script>
---
+++ config/headerAuxiliar.php
@@ -0,0 +1,9 @@
+<div class="cont">
+    <div class="tresLineas">
+        <img src="../../Imagenes/Iconos/TresLineasBlancas.png" alt="Barra desplegable" width="50px" height="50px">
+    </div>
+    <div class="logo">
+        <img src="../../Imagenes/Iconos/tucan.png" alt="Logo" width="40px" height="40px" class="d-inline-block align-text-top">
+        <h1 class="titulo_logotipo">Zoologics</h1>
+    </div>
+</div>
